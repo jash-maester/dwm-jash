c33cd90f095cf380762f81249234585e28fa0211
Jashaswimalya Acharjee <jashaswimalyaacharjee@gmail.com>
2021-04-14 14:12:52 +0530

    added cursor-warp patch

--- dwm.c
+++ dwm.c
@@ -1070,6 +1070,8 @@ focusmon(const Arg *arg)
 	unfocus(selmon->sel, 0);
 	selmon = m;
 	focus(NULL);
+	if (selmon->sel)
+		XWarpPointer(dpy, None, selmon->sel->win, 0, 0, 0, 0, selmon->sel->w/2, selmon->sel->h/2);
 }
 
 void
@@ -1095,6 +1097,7 @@ focusstack(const Arg *arg)
 	if (c) {
 		focus(c);
 		restack(selmon);
+		XWarpPointer(dpy, None, c->win, 0, 0, 0, 0, c->w/2, c->h/2);
 	}
 }
 
